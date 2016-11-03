2f25a45ebc3237a2d5a20e36142eacd08dc703a0
William Cody Laeder <codylaeder@gmail.com>
2016-11-03 02:08:27 +0000

    Added docs in pre for 0.0.2 release.

--- docs/implementors/core/clone/trait.Clone.js
+++ docs/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["context_bind"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='context_bind/enum.StackSize.html' title='context_bind::StackSize'>StackSize</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='context_bind/enum.Status.html' title='context_bind::Status'>Status</a>",];
+implementors["context_bind"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='context_bind/enum.StackSize.html' title='context_bind::StackSize'>StackSize</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- docs/implementors/core/fmt/trait.Debug.js
+++ docs/implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["context_bind"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='context_bind/enum.StackSize.html' title='context_bind::StackSize'>StackSize</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='context_bind/enum.Status.html' title='context_bind::Status'>Status</a>",];
+implementors["context_bind"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='context_bind/enum.StackSize.html' title='context_bind::StackSize'>StackSize</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
