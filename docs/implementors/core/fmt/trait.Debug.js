013c8e28a066bd0106e14a521143e7a7e89be2f5
William Cody Laeder <codylaeder@gmail.com>
2016-11-03 00:57:54 +0000

    Added docs.

---
+++ docs/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["context_bind"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='context_bind/enum.StackSize.html' title='context_bind::StackSize'>StackSize</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='context_bind/enum.Status.html' title='context_bind::Status'>Status</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
